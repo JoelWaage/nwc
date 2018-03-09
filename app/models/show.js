ac3e0e0f50c9a1cdb78c42c6d54881c2be401131
Joel <joeltwaage@gmail.com>
2018-03-09 15:13:19 -0500

    expand model to include music, lyrics, book and blurb

--- app/models/show.js
+++ app/models/show.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  poster: DS.attr()
+  music: DS.attr(),
+  lyrics: DS.attr(),
+  book: DS.attr(),
+  blurb: DS.attr(),
+  image: DS.attr()
 });
