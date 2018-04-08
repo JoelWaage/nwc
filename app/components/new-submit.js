808d69bc326c935083a3f556a669832327f7d3d0
Joel <joeltwaage@gmail.com>
2018-04-08 10:38:12 -0400

    create calendar component and basic calendar layout

--- app/components/new-submit.js
+++ app/components/new-submit.js
@@ -1,5 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-
+  actions: {
+    saveSubmit1() {
+      var params = {
+        contact: this.get('contact'),
+        submitDate: this.get('submitDate'),
+        performDates: this.get('performDates'),
+        title: this.get('title'),
+        music: this.get('music'),
+        lyrics: this.get('lyrics'),
+        book: this.get('book'),
+        blurb: this.get('blurb'),
+        image: this.get('image')
+      };
+      this.set('addNewSubmit', false);
+      this.sendAction('saveSubmit2', params);
+      console.log(params);
+    }
+  }
 });
